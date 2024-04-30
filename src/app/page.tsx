78a86f8219fabab9b305fc67d6a486a6fa56dcdb
Megan Chantosa <meganemmamoore@gmail.com>
2024-04-29 19:41:20 -0700

    Refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
 import Navigation from "./Navigation";
 import Hero from "./Hero";
-import About from "./about";
-import Resume from "./resume";
+import About from "./About";
+import Resume from "./Resume";
 import { useRef } from "react";
 
 export default function Home() {
+  const sectionTailwind: {
+    section: string[];
+    sectionTitle: string[];
+    sectionTitleAfter: string[];
+  } = {
+    section: ["py-16", "overflow-hidden", "background: #f5f8fd;"],
+    sectionTitle: [
+      "pb-8",
+      "text-2xl",
+      "font-bold",
+      "mb-5",
+      "pb-5",
+      "relative",
+      "text-blue-800",
+    ],
+    sectionTitleAfter: [
+      "absolute",
+      "block",
+      "w-50",
+      "h-3",
+      "bg-blue-500",
+      "bottom-0",
+      "left-0",
+    ],
+  };
   return (
     <>
       <Navigation /> {/* */}
